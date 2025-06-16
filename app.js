1a3ed5f3cf729a1ad049577b5803d5ac4b9f493e
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-16 15:53:38 +0200

    add middlewares, body parser and static assets

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+// config
 const express = require("express");
 const app = express();
 
+// import
 require("dotenv").config();
-
 const { APP_PORT, APP_URL } = process.env;
+const { notFound, errorHandler } = require("./middlewares/error");
 
+// static assets
+app.use(express.static("public"));
+
+// body parser
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("ok");
+});
+
+//middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+// listen
 app.listen(3000, () => {
   console.log(`In ascolto su ${APP_URL}: ${APP_PORT}`);
 });

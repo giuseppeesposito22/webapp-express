08aba45a8b5a147f22814e416896bc2dd6125b1c
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-19 15:45:17 +0200

    add store in routers

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -9,4 +9,7 @@ router.get("/", movieController.index);
 // show
 router.get("/:id", movieController.show);
 
+// store
+router.post("/:id/review", movieController.storeReview);
+
 module.exports = router;

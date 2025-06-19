dedfed41dcf03aff52b4283a5ba09aad5c83515c
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-19 15:45:36 +0200

    add store review in controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -41,6 +41,45 @@ const show = (req, res) => {
   });
 };
 
+const storeReview = (req, res) => {
+  const { id } = req.params;
+
+  const { name, vote, text } = req.body;
+
+  const dataValues = [id, name, vote, text];
+
+  let errors = [];
+
+  if (!vote || vote < 1 || vote > 5) {
+    errors.push({ message: "Il voto deve essere compreso tra 1 e 5" });
+  }
+
+  if (!name) {
+    errors.push({ message: "Nome obbligatorio" });
+  }
+
+  if (!text || text.length < 5) {
+    errors.push({
+      message: "Inserire una recensione con lunghezza minima di 5 lettere",
+    });
+  }
+
+  if (errors.length > 0) {
+    return res.status(402).json({ errors });
+  }
+
+  const query =
+    "INSERT INTO film.reviews (movie_id, name, vote, text) VALUES (?, ?, ?, ?)";
+
+  connection.query(query, dataValues, (err, results) => {
+    if (err) return res.status(500), json({ message: "Query failed", err });
+
+    res.status(201).json({ message: "review added successfully" });
+
+    console.log(results);
+  });
+};
+
 // metodo per trasformare il path dell'immagine di ogni film
 const pathImage = (movie) => {
   movie.image = `http://localhost:3000/img/movies_cover/${movie.image}`;
@@ -48,4 +87,4 @@ const pathImage = (movie) => {
   return movie;
 };
 
-module.exports = { index, show };
+module.exports = { index, show, storeReview };
